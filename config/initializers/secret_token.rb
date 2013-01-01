9f321e8cf771e327dcb9d5834f69a223da5f406d
wuwx <wuweixin@gmail.com>
2013-01-01 15:46:25 +0800

    Rails 3.2.9

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Leng::Application.config.secret_token = '699756430fbe19e295c41a5e78be001494013a9fb679e69dd6ff044a6c961cf077e5c88ac17ff688430b1195753f75ba81b29079d61ff5fbf93513e41bb608b6'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Leng::Application.config.session_store :cookie_store, key: '_leng_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Leng::Application.config.session_store :active_record_store
