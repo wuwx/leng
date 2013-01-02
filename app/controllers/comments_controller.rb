8812890e3ca8c4e7927ab10e72f59e3a5e01325b
wuwx <wuweixin@gmail.com>
2013-01-02 22:04:32 +0800

    add index action to comments controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,12 @@
 class CommentsController < ApplicationController
+  def index
+    @topic = Topic.find(params[:topic_id])
+    @comments = @topic.comments
+    
+    respond_to do |format|
+      format.html # index.html.erb
+      format.json { render json: @topics }
+      format.js
+    end
+  end
 end
