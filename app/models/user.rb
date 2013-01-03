280cfb04c1a61a2900cc18511872ffd9f0151056
wuwx <wuweixin@gmail.com>
2013-01-03 17:14:12 +0800

    user validates

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,9 @@
 class User < ActiveRecord::Base
   has_secure_password
   attr_accessible :name, :email, :password, :password_confirmation
+  
+  validates :name, :presence => true
+  validates :email, :presence => true
+  validates :password, :presence => true
+  validates :password_confirmation, :presence => true
 end
