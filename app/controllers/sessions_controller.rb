16f38fa7edf59116c9d3998a30a969c785004091
wuwx <wuweixin@gmail.com>
2013-01-03 18:20:09 +0800

    sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,11 +5,20 @@ class SessionsController < ApplicationController
   end
   
   def create
+    @session = Session.new(params[:session])
     
+    if @session.save
+      session[:user_id] = @session.user_id
+      redirect_to root_path, :notice => "Logged in!"
+    else
+      flash.now.alert = "Invalid email or password"
+      render "new"
+    end
   end
   
   def destroy
-    
+    session[:user_id] = nil
+    redirect_to root_path, :notice => "Logged out!"
   end
   
 end
