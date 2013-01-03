7184c8604b33044766385fa973705104b163e063
wuwx <wuweixin@gmail.com>
2013-01-03 21:33:34 +0800

    comment 必须由某用户发布

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,4 +2,7 @@ class Comment < ActiveRecord::Base
   attr_accessible :content
   belongs_to :topic
   belongs_to :user
+  
+  validates :topic_id, :presence => true
+  validates :user_id, :presence => true
 end
