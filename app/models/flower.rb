e95147873d03caf8f5c003048db21643fb0ac5e5
wuwx <wuweixin@gmail.com>
2013-01-04 12:03:18 +0800

    一个人不能喜欢一个主题两次

--- app/models/flower.rb
+++ app/models/flower.rb
@@ -1,4 +1,6 @@
 class Flower < ActiveRecord::Base
   belongs_to :topic, :counter_cache => true
   belongs_to :user, :counter_cache => true
+  
+  validates :user_id, :uniqueness => { :scope => :topic_id }
 end
