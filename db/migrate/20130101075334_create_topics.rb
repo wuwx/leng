e44451c741bf61abc77d47626673e439df84a793
wuwx <wuweixin@gmail.com>
2013-01-04 12:05:56 +0800

    设置默认的counter cache为0

--- db/migrate/20130101075334_create_topics.rb
+++ db/migrate/20130101075334_create_topics.rb
@@ -2,7 +2,7 @@ class CreateTopics < ActiveRecord::Migration
   def change
     create_table :topics do |t|
       t.text :content
-      t.integer :flowers_count
+      t.integer :flowers_count, :default => 0
       t.references :user
       t.timestamps
     end
--- db/migrate/20130102054810_create_users.rb
+++ db/migrate/20130102054810_create_users.rb
@@ -4,7 +4,7 @@ class CreateUsers < ActiveRecord::Migration
       t.string :name
       t.string :email
       t.string :password_digest
-      t.integer :flowers_count
+      t.integer :flowers_count, :default => 0
       t.timestamps
     end
   end
