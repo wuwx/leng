cde565968f3cb54dfb7d7cab38caecffea25c728
wuwx <wuweixin@gmail.com>
2013-01-04 11:59:41 +0800

    喜欢按钮以及喜欢的操作

--- app/controllers/flowers_controller.rb
+++ app/controllers/flowers_controller.rb
@@ -1,2 +1,17 @@
 class FlowersController < ApplicationController
+  
+  def create
+    @topic = Topic.find(params[:topic_id])
+    @flower = @topic.flowers.build
+    @flower.user = current_user
+    
+    respond_to do |format|
+      if @flower.save
+        format.js
+      else
+        format.js
+      end
+    end
+  end
+  
 end
