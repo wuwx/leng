0ff2127e1d911e6550a2accb5242cf0dab80081d
wuwx <wuweixin@gmail.com>
2013-01-06 14:05:32 +0800

    Devise的侵入性太强了

--- db/migrate/20130102054810_create_users.rb
+++ db/migrate/20130102054810_create_users.rb
@@ -1,13 +1,17 @@
 class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
-      t.string :name
-      t.string :email
-      t.string :password_digest
+      t.string :name, :null => false
+      t.string :email, :null => false, :default => ""
+      t.string :password_digest, :null => false
+      
       t.integer :comments_count, :default => 0
       t.integer :flowers_count, :default => 0
       t.integer :topics_count, :default => 0
       t.timestamps
     end
+    
+    add_index :users, :name, :unique => true
+    add_index :users, :email, :unique => true
   end
 end
