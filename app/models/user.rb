c8931171e91245399169bf2231a19c7ebf204055
wuwx <wuweixin@gmail.com>
2013-01-06 14:07:49 +0800

    Devise的侵入性太强了

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,5 @@
 class User < ActiveRecord::Base
-  # Include default devise modules. Others available are:
-  # :token_authenticatable, :confirmable,
-  # :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
-
-  # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me
+  
   include Gravtastic
   gravtastic
   has_secure_password
@@ -21,4 +14,5 @@ class User < ActiveRecord::Base
   validates :email, :presence => true
   validates :password, :presence => true
   validates :password_confirmation, :presence => true
+  
 end
