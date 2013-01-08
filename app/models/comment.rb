ab3a39e34abf86d53b467c3b92f81838a0bef63c
wuwx <wuweixin@gmail.com>
2013-01-09 00:57:42 +0800

    只能提交允许的数据

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,5 @@
 class Comment < ActiveRecord::Base
+  include ActiveModel::ForbiddenAttributesProtection
   attr_accessible :content
   belongs_to :topic, :counter_cache => true
   belongs_to :user, :counter_cache => true
--- app/models/flower.rb
+++ app/models/flower.rb
@@ -1,4 +1,5 @@
 class Flower < ActiveRecord::Base
+  include ActiveModel::ForbiddenAttributesProtection
   belongs_to :topic, :counter_cache => true
   belongs_to :user, :counter_cache => true
   
