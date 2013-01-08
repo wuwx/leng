d2f72a7e1be7afd7378a4ba38c8d36d2be858bc4
wuwx <wuweixin@gmail.com>
2013-01-08 13:51:05 +0800

    重写render_page_title

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  
+  def render_page_title
+    site_name = Setting.app_name
+    title = @page_title ? "#{site_name} | #{@page_title}" : site_name rescue "SITE_NAME"
+    content_tag("title", title, nil, false)
+  end
+  
 end
