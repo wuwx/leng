b4c0b9d453393597a8d442eb6652dc3fc115fe05
wuwx <wuweixin@gmail.com>
2013-01-09 01:24:17 +0800

    可以使用devise的current_user方法

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,9 +5,4 @@ class ApplicationController < ActionController::Base
   before_filter do
     @current = []
   end
-  
-  private
-  def current_user
-    @current_user ||= User.find_by_id(session[:user_id]) if session[:user_id]
-  end
 end
