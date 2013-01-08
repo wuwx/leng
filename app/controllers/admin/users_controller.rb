26627c73f6dd4b61a4e57a5f906b52026dc5cd08
wuwx <wuweixin@gmail.com>
2013-01-08 09:17:03 +0800

    参数的设置

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -5,4 +5,12 @@ class Admin::UsersController < Admin::ApplicationController
     def collection
       @users ||= end_of_association_chain.page(params[:page]).reverse_order
     end
+    
+    def resource_params
+      [params.require(:user).permit(:name, :email, :password, :password_confirmation, :role_ids)] unless request.get?
+    end
+    
+    def resource_url
+      [:edit, :admin, resource]
+    end
 end
