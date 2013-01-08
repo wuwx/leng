77e6678818ca54c3774de8df116486457f9e7312
wuwx <wuweixin@gmail.com>
2013-01-08 16:02:23 +0800

    后台管理权限

--- app/controllers/admin/permissions_controller.rb
+++ app/controllers/admin/permissions_controller.rb
@@ -1,2 +1,13 @@
-class Admin::PermissionsController < ApplicationController
+class Admin::PermissionsController < Admin::ApplicationController
+  
+  inherit_resources
+  
+  def resource_params
+    [params.require(:permission).permit(:name, :resource, :action, :role_ids)] unless request.get?
+  end
+  
+  def resource_url
+    [:edit, :admin, resource]
+  end
+  
 end
