47c5c014df8b7d33a13710ddb64022641ed2c97f
wuwx <wuweixin@gmail.com>
2013-01-08 09:05:38 +0800

    使用后台提交的参数经过检查

--- app/controllers/admin/roles_controller.rb
+++ app/controllers/admin/roles_controller.rb
@@ -1,3 +1,12 @@
 class Admin::RolesController < Admin::ApplicationController
   inherit_resources
+  
+  def resource_params
+    [params.require(:role).permit(:name)] unless request.get?
+  end
+  
+  def resource_url
+    [:edit, :admin, resource]
+  end
+  
 end
