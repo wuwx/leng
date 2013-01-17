6b1201be038c9691075d26b92988f306c57e05e0
wuwx <wuweixin@gmail.com>
2013-01-17 10:47:39 +0800

    acts_as_commentable

--- db/migrate/20130117024534_create_comments.rb
+++ db/migrate/20130117024534_create_comments.rb
@@ -1,8 +1,9 @@
 class CreateComments < ActiveRecord::Migration
   def change
     create_table :comments do |t|
-      t.text :content
-      t.references :topic
+      t.string :title, :limit => 50, :default => "" 
+      t.text :comment
+      t.references :commentable, :polymorphic => true
       t.references :user
       t.timestamps
     end
