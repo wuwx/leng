38853e2bdd98b055cd6e9748d16c8be12a8fe350
wuwx <wuweixin@gmail.com>
2013-01-19 22:32:20 +0800

    admin/topics test

--- app/controllers/admin/topics_controller.rb
+++ app/controllers/admin/topics_controller.rb
@@ -8,8 +8,17 @@ class Admin::TopicsController < Admin::ApplicationController
     drop_page_title("主题管理")
   end
   
+  def create
+    build_resource.user = current_user
+    create!
+  end
+  
   protected
     def collection
       @topics ||= end_of_association_chain.page(params[:page]).reverse_order
+    end
+    
+    def resource_params
+      [params.require(:topic).permit(:content)] unless request.get?
     end
 end
--- test/functional/admin/topics_controller_test.rb
+++ test/functional/admin/topics_controller_test.rb
@@ -1,7 +1,51 @@
 require 'test_helper'
 
 class Admin::TopicsControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @user = FactoryGirl.create(:user)
+    @topic = FactoryGirl.create(:topic)
+    sign_in @user
+  end
+
+  test "should get index" do
+    get :index
+    assert_response :success
+    assert_not_nil assigns(:topics)
+  end
+
+  test "should get new" do
+    get :new
+    assert_response :success
+  end
+
+  test "should create topic" do
+    assert_difference('Topic.count') do
+      post :create, topic: { 'content' => 1 }
+    end
+
+    assert_redirected_to admin_topic_path(assigns(:topic))
+  end
+
+  test "should show topic" do
+    get :show, id: @topic
+    assert_response :success
+  end
+
+  test "should get edit" do
+    get :edit, id: @topic
+    assert_response :success
+  end
+
+  test "should update topic" do
+    put :update, id: @topic, topic: { content: 1 }
+    assert_redirected_to admin_topic_path(assigns(:topic))
+  end
+
+  test "should destroy topic" do
+    assert_difference('Topic.count', -1) do
+      delete :destroy, id: @topic
+    end
+
+    assert_redirected_to admin_topics_path
+  end
 end
