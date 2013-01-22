c83f7454bd49d0298f750378e83784e326896aa1
wuwx <wuweixin@gmail.com>
2013-01-22 20:20:43 +0800

    允许用户评论

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -30,6 +30,6 @@ class CommentsController < ApplicationController
   
   private
     def comment_params
-      params.require(:comment).permit(:content)
+      params.require(:comment).permit(:comment)
     end
 end
